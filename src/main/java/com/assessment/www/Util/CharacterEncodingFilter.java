3642fb2df4bc6452c2c85bf944e111bd139c2e79
daydream <3380971468@example.com>
2026-04-15 23:06:32 +0800

    last_one

--- src/main/java/com/assessment/www/Util/CharacterEncodingFilter.java
+++ src/main/java/com/assessment/www/Util/CharacterEncodingFilter.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class CharacterEncodingFilter implements Filter {
     private String encoding = "UTF-8";
 
-    // 写了一个默认utf-8过滤器 利用的是tc会自动读取web.xml中的filter配置
+    // 写了一个默认utf-8过滤器 利用的是tc会自动读取web.xml
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         String encodingParam = filterConfig.getInitParameter("encoding");
@@ -21,7 +21,7 @@ public class CharacterEncodingFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
-        // 再者 通过将请求在到达servlet之前 换编码为utf-8
+        //通过将请求在到达servlet之前 换编码为utf-8
         request.setCharacterEncoding(encoding);// 设置请求编码
         response.setCharacterEncoding(encoding);// 设置响应编码
         response.setContentType("text/html;charset=" + encoding);//请求头设置响应编码
